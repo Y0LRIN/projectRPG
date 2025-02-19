12b1d4b25af04875939535c5a381002ef7b9f578
Chloe Alexandre <chloe.alexandre@ynov.com>
2025-02-19 16:07:39 +0100

    Menu with string[]

--- Menu.ts
+++ Menu.ts
@@ -2,27 +2,13 @@ class menu {
     //Properties
     public menuID: number;
     public menuDisplay: string;
-    public pos1: string;
-    public pos2: string;
-    public pos3: string;
-    public pos4: string;
-    public pos5: string;
-    public pos6: string;
-    public pos7: string;
-    public pos8: string;
+    public options: string[];
 
     //Constructor
-    constructor(menuID: number, menuDisplay: string, pos1: string, pos2: string, pos3: string, pos4: string, pos5: string, pos6: string, pos7: string, pos8: string) {
+    constructor(menuID: number, menuDisplay: string, options: string[]) {
         this.menuID = menuID;
         this.menuDisplay = menuDisplay;
-        this.pos1 = pos1;
-        this.pos2 = pos2;
-        this.pos3 = pos3;
-        this.pos4 = pos4;
-        this.pos5 = pos5;
-        this.pos6 = pos6;
-        this.pos7 = pos7;
-        this.pos8 = pos8;
+        this.options = options;
     }
 
     public displayMenu() {
@@ -30,14 +16,11 @@ class menu {
     }
 
     public displayOptions() {
-        console.log(`1. ${this.pos1}`);
-        console.log(`2. ${this.pos2}`);
-        console.log(`3. ${this.pos3}`);
-        console.log(`4. ${this.pos4}`);
-        console.log(`5. ${this.pos5}`);
-        console.log(`6. ${this.pos6}`);
-        console.log(`7. ${this.pos7}`);
-        console.log(`8. ${this.pos8}`);
+        for (let i = 0; i<this.options.length; i++) {
+            if (this.options[i] !== "") {
+                console.log(`${i+1}. ${this.options[i]}`);
+            }
+        }
     }
 
     public selectOption() {
