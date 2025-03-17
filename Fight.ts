b07e75cacf39c64703d0a5fa7158663f1d081d17
Chloe Alexandre <chloe.alexandre@ynov.com>
2025-03-17 09:39:03 +0100

    Implement basic fight mechanics with turn order and end conditions

---
+++ Fight.ts
@@ -0,0 +1,53 @@
+import { Character } from './Character';
+
+class Fight {
+    private team: Character[];
+    private enemies: Character[];
+    private turnOrder: Character[];
+    
+    public constructor(team: Character[], enemies: Character[]) {
+        this.team = team;
+        this.enemies = enemies;
+        this.turnOrder = [...team, ...enemies].sort((a, b) => b.SPD - a.SPD);
+    }
+
+    public start() {
+        console.log('The fight begins!');
+        this.fightTurn(0);
+    }
+
+    private fightTurn(index: number) {
+        if (this.checkEnd()) {
+            return;
+        }
+        if (index >= this.turnOrder.length) {
+            this.fightTurn(0);
+            return;
+        }
+        const character = this.turnOrder[index];
+        if (character.isAlive()) {
+            if (this.team.includes(character)) {
+                this.teamTurn(character);
+            } else {
+                this.enemyTurn(character);
+            }
+        }
+        this.fightTurn(index + 1);
+    }
+
+    private teamTurn(character: Character) {}
+
+    private enemyTurn(character: Character) {}
+
+    private checkEnd() {
+        if (this.team.every(character => !character.isAlive())) {
+            console.log('The fight is over! The enemies win!');
+            return true;
+        }
+        if (this.enemies.every(character => !character.isAlive())) {
+            console.log('The fight is over! The team wins!');
+            return true;
+        }
+        return false;
+    }
+}
