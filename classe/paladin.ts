cc196185c422bad244f73e36b8fa987491d9c6aa
Chloe Alexandre <chloe.alexandre@ynov.com>
2025-03-17 11:25:42 +0100

    Implement Paladin class with smite ability and refactor Warrior class to use smite; add Fight class for combat management

--- classe/paladin.ts
+++ classe/paladin.ts
@@ -1,7 +1,26 @@
 import { Character } from "../Character";
 
-class Paladin {
+class Paladin extends Character {
     constructor(name: string) {
-        super(name, )
+        super(name, 7, 12, 8, 20, 25, 25);
+    }
+
+    public smite(targets: Character[]) {
+        if (!this.isAlive()) {
+            console.log(`${this.name} is dead and cannot attack!`);
+            return;
+        }
+        const aliveEnemies = targets.filter(enemy => enemy.isAlive());
+        if (aliveEnemies.length === 0) {
+            console.log(`There are no alive enemies to attack!`);
+            return;
+        }
+        const targetEnemy = aliveEnemies[Math.floor(Math.random() * aliveEnemies.length)];
+        let damage = this.ATK * 1.5 - targetEnemy.DEF;
+        targetEnemy.currenthealth -= damage;
+        if (targetEnemy.currenthealth < 0) {
+            targetEnemy.currenthealth = 0;
+        }
+        console.log(`${this.name} smites ${targetEnemy.name} for ${damage} damage!`);
     }
 }
--- classe/warrior.ts
+++ classe/warrior.ts
@@ -5,7 +5,7 @@ class Warrior extends Character {
         super(name, 10, 3, 12, 20, 25, 25);
     }
     
-    public berserk(enemies: Character[]) {
+    public smite(enemies: Character[]) {
         if (!this.isAlive()) {
             console.log(`${this.name} is dead and cannot attack!`);
             return;
@@ -15,18 +15,13 @@ class Warrior extends Character {
             console.log(`There are no alive enemies to attack!`);
             return;
         }
-        const target = aliveEnemies[Math.floor(Math.random() * aliveEnemies.length)];
-        let damage = this.ATK * 1.3 - target.DEF;
-        target.currenthealth -= damage;
-        if (target.currenthealth < 0) {
-            target.currenthealth = 0;
+        for (const target of aliveEnemies) {
+            let damage = (this.ATK - target.DEF) * 0.4;
+            target.currenthealth -= damage;
+            if (target.currenthealth < 0) {
+                target.currenthealth = 0;
+            }
+            console.log(`${this.name} smites ${target.name} for ${damage} damage!`);
         }
-        let selfDamage = this.maxHealth * 0.2;
-        this.currenthealth -= selfDamage;
-        if (this.currenthealth < 0) {
-            this.currenthealth = 0;
-        }
-        console.log(`${this.name} hits ${target.name} for ${damage} damage!`);
-        console.log(`${this.name} takes ${selfDamage} damage from the attack!`);
     }
 }
