6538c1765ae6ce8bf8645fd60bd86409023bcbe7
Adrianne98 <jaworski.adrian44@gmail.com>
2025-03-22 16:32:42 +0100

    caster bientot fini

--- classe/caster.ts
+++ classe/caster.ts
@@ -12,9 +12,37 @@ class Caster extends Character {
         this.attackMagic = 10;
 
     }
-
-
-
+public attack(target: Character): void {
+    if (!this.isAlive()) {
+        console.log(`${this.name} is dead and cannot attack!`);
+        return;
+    }
+    if (!target.isAlive()) {
+        console.log(`${target.name} is already dead!`);
+        return;
+    }
     
+    let damage = Math.max(this.ATK - target.DEF, 0);
+     let result = target.currenthealth - damage;
+    if (result < 0) {
+        target.currenthealth = 0;
+    }
+    else {
+        target.currenthealth = result;
+    }
+    console.log(`${this.name} attacks ${target.name} for ${damage} damage!`);
+
 }
 
+public DamageMagic(target: Character): void {
+    if (!this.isAlive()) {
+        console.log(`${this.name} is dead and cannot attack!`);
+        return;
+    }
+    if (!target.isAlive()) {
+        console.log(`${target.name} is already dead!`);
+        return;
+    }
+    
+}
+}
