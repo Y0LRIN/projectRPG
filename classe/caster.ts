917e9f1887bb7dfa3b7edb4f8859a96360111ddb
Adrianne98 <jaworski.adrian44@gmail.com>
2025-03-22 15:40:27 +0100

    classe tj encourt

--- classe/caster.ts
+++ classe/caster.ts
@@ -3,10 +3,18 @@ import { Character } from '../Character';
 class Caster extends Character {
     private maxMana : Number;
     private currentMana : Number;  
+    private attackMagic : Number;
+
     constructor() {
-        super('Clere', 4, 5, 10, 25, 20, 20,); 
+        super('Caster', 4, 5, 10, 25, 20, 20,); 
         this.maxMana = 20;
         this.currentMana = 20;
+        this.attackMagic = 10;
 
     }
+
+
+
+    
 }
+
---
+++ classe/knight.ts
@@ -0,0 +1,7 @@
+import { Character } from "../Character";
+
+class Knight extends Character {
+    constructor() {
+        super('Knight', 10, 10, 10, 20, 20, 20);
+    }
+}
