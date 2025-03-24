8cf831fc82dc9d05d1f36fb6cc332adcc3afb1d1
Adrianne98 <jaworski.adrian44@gmail.com>
2025-03-24 09:10:10 +0100

    chevalier fini

--- classe/knight.ts
+++ classe/knight.ts
@@ -4,4 +4,20 @@ class Knight extends Character {
     constructor() {
         super('Knight', 10, 10, 10, 20, 20, 20);
     }
+    public attack(target: Character) {
+        if (!this.isAlive()) {
+            console.log(`${this.name} is dead and cannot attack!`);
+            return;
+        }
+        if (!target.isAlive()) {
+            console.log(`${target.name} is dead and cannot be attacked!`);
+            return;
+        }
+        let damage = this.ATK - target.DEF;
+        target.currenthealth -= damage;
+        if (target.currenthealth < 0) {
+            target.currenthealth = 0;
+        }
+        console.log(`${this.name} attacks ${target.name} for ${damage} damage!`);
+    }
 }
