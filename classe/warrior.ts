06afdfc97dd8273f0401f155db19e439c47b0080
Chloe Alexandre <chloe.alexandre@ynov.com>
2025-03-31 11:23:05 +0200

    rename smite method to berserk and update damage calculation

--- classe/warrior.ts
+++ classe/warrior.ts
@@ -5,7 +5,7 @@ class Warrior extends Character {
         super(name, 10, 3, 12, 20, 25, 25);
     }
     
-    public smite(enemies: Character[]) {
+    public berserk(enemies: Character[]) {
         if (!this.isAlive()) {
             console.log(`${this.name} is dead and cannot attack!`);
             return;
@@ -15,13 +15,17 @@ class Warrior extends Character {
             console.log(`There are no alive enemies to attack!`);
             return;
         }
-        for (const target of aliveEnemies) {
-            let damage = (this.ATK - target.DEF) * 0.4;
-            target.currenthealth -= damage;
-            if (target.currenthealth < 0) {
-                target.currenthealth = 0;
-            }
-            console.log(`${this.name} smites ${target.name} for ${damage} damage!`);
+        const target = aliveEnemies[Math.floor(Math.random() * aliveEnemies.length)];
+        let damage = this.ATK * 1.3 - target.DEF;
+        target.currenthealth -= damage;
+        if (target.currenthealth < 0) {
+            target.currenthealth = 0;
         }
+        this.currenthealth -= this.maxHealth * 0.2;
+        if (this.currenthealth < 0) {
+            this.currenthealth = 0;
+        }
+        console.log(`${this.name} berserks and attacks ${target.name} for ${damage} damage!`);
+        console.log(`${this.name} takes 20% of max health as damage!`);
     }
 }
