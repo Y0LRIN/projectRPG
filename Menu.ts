4c05bf11bb244992b62f1787dc2f81df94c9d0cb
Adrianne98 <jaworski.adrian44@gmail.com>
2025-04-01 17:22:19 +0200

    mise en page francaise

--- Menu.ts
+++ Menu.ts
@@ -2,7 +2,9 @@ class menu {
     //Properties
     private menuID: number;
     private menuDisplay: string;
-    private options: string[];
+    protected options: string[];
+    private selectedOption: string;
+    private selectedOptionHeros: string[];
 
     public getMenuID(): number {
         return this.menuID;
@@ -32,19 +34,27 @@ class menu {
 
     //Method that selects an option
     public selectOption() {
-        let option = prompt("Select an option: ");
+        let option = prompt("Selectionnez une option: ");
         if (option === null || !option.match(/^[1-8]$/) || option === "") {
-            console.log("Invalid option. Please select a valid option.");
+            console.log("Option Invalide.Slectionnez une option valide.");
+            return this.selectOption();
+        }
+        return option;
+    }
+    public selectOptionHeros() {
+        let option = prompt("Select an option: ");
+        if (option === null || !option.match(/^[1-6]$/) || option === "") {
+            console.log("Option Invalide.Slectionnez une option valide.");
             return this.selectOption();
         }
         return option;
     }
 
 }
-const Mainmenu = new menu(1, "Main Menu", ["Start Game","Exit Game"])
+const Mainmenu = new menu(1, "Main Menu", ["Jouer","Quitter"]);
 
 console.log("#///////////////////////////////////////////////////////////////#");
-console.log(" #                      Welcome to the                         #");
+console.log(" #                      Bienvenue dans                         #");
 console.log(" #                           RPG                               #");
 console.log(" #                       Tape Goblin                           #");
 console.log("#///////////////////////////////////////////////////////////////#");
@@ -53,7 +63,7 @@ Mainmenu.displayOptions();
 const selectedOption = Mainmenu.selectOption();
 if (selectedOption === "1") {}
 else if (selectedOption === "2") {console.log("Merci d'avoir joué à notre jeu !");
-    process.exit(0);}
+    Deno.exit(0);}
 
 console.log("#///////////////////////////////////////////////////////////////#")
 const Charatermenu = new menu(2, "#                            Heros                              #",
@@ -63,19 +73,17 @@ const Charatermenu = new menu(2, "#                            Heros            
     console.log("#///////////////////////////////////////////////////////////////#")
     Charatermenu.displayMenu();
     Charatermenu.displayOptions();
-    const selectedOptionHeros1 = Charatermenu.selectOption();
-    const selectedOptionHeros2 = Charatermenu.selectOption();
-    const selectedOptionHeros3 = Charatermenu.selectOption();
+    const selectedOptionHeros1 = Charatermenu.selectOptionHeros();
+    const selectedOptionHeros2 = Charatermenu.selectOptionHeros();
+    const selectedOptionHeros3 = Charatermenu.selectOptionHeros();
     console.log("#///////////////////////////////////////////////////////////////#")
     console.log("#                                                               #")
     console.log("#             vous avez choisi les heros suivants :             #")
     console.log("#                                                               #")
     console.log("#///////////////////////////////////////////////////////////////#")
-    console.log(`Heros 1 : ${Charatermenu.options[selectedOptionHeros1-1]}`)
-    console.log(`Heros 2 : ${Charatermenu.options[selectedOptionHeros2-1]}`)
-    console.log(`Heros 3 : ${Charatermenu.options[selectedOptionHeros3-1]}`)
-
-function seletHeros(Charatermenu:string) {
+    console.log(`Heros 1 : ${Charatermenu.options[parseInt(selectedOptionHeros1)-1]}`);
+    console.log(`Heros 2 : ${Charatermenu.options[parseInt(selectedOptionHeros2)-1]}`);
+    console.log(`Heros 3 : ${Charatermenu.options[parseInt(selectedOptionHeros3)-1]}`);
+    console.log("#///////////////////////////////////////////////////////////////#")
     
-}
 
