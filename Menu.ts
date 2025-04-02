250b804ef8290ed9e509ff5f9fca39c4545bd06d
Adrianne98 <jaworski.adrian44@gmail.com>
2025-04-02 13:49:12 +0200

    .

--- Menu.ts
+++ Menu.ts
@@ -41,14 +41,20 @@ class menu {
         }
         return option;
     }
+
     public selectOptionHeros() {
         let option = prompt("Select an option: ");
         if (option === null || !option.match(/^[1-6]$/) || option === "") {
             console.log("Option Invalide.Slectionnez une option valide.");
+            if (this.options.includes(selectedOptionHeros)) {
+                console.log("Vous avez déjà choisi ce héros.");
+            }
             return this.selectOption();
         }    
         return option;
     }
+    
+
 
 }
 
@@ -59,6 +65,7 @@ console.log(" #                      Bienvenue dans                         #");
 console.log(" #                           RPG                               #");
 console.log(" #                       Tape Goblin                           #");
 console.log("#///////////////////////////////////////////////////////////////#");
+
 Mainmenu.displayMenu();
 Mainmenu.displayOptions();
 const selectedOption = Mainmenu.selectOption();
@@ -66,17 +73,19 @@ if (selectedOption === "1") {}
 else if (selectedOption === "2") {console.log("Merci d'avoir joué à notre jeu !");
     Deno.exit(0);}
 
-console.log("#///////////////////////////////////////////////////////////////#")
+    console.log("#///////////////////////////////////////////////////////////////#")
 const Charatermenu = new menu(2, "#                            Heros                              #",
     ["Guerrier","Mage","Paladin","Barbare","Prêtre","Voleur"]);
     console.log("#                 choisie ton équipe de heros                   #")
     console.log("#                                                               #")
     console.log("#///////////////////////////////////////////////////////////////#")
-    Charatermenu.displayMenu();
-    Charatermenu.displayOptions();
-    const selectedOptionHeros1 = Charatermenu.selectOptionHeros();
-    const selectedOptionHeros2 = Charatermenu.selectOptionHeros();
-    const selectedOptionHeros3 = Charatermenu.selectOptionHeros();
+Charatermenu.displayMenu();
+Charatermenu.displayOptions();
+
+const selectedOptionHeros1 = Charatermenu.selectOptionHeros();
+const selectedOptionHeros2 = Charatermenu.selectOptionHeros();
+const selectedOptionHeros3 = Charatermenu.selectOptionHeros();
+
     console.log("#///////////////////////////////////////////////////////////////#")
     console.log("#                                                               #")
     console.log("#             vous avez choisi les heros suivants :             #")
@@ -86,5 +95,6 @@ const Charatermenu = new menu(2, "#                            Heros            
     console.log(`Heros 2 : ${Charatermenu.options[parseInt(selectedOptionHeros2)-1]}`);
     console.log(`Heros 3 : ${Charatermenu.options[parseInt(selectedOptionHeros3)-1]}`);
     console.log("#///////////////////////////////////////////////////////////////#")
-    
+
+
 
