ec996f557758dbc72c69c533529bbe6a186a9e32
Adrianne98 <jaworski.adrian44@gmail.com>
2025-04-03 21:56:17 +0200

    import dans test

---
+++ test.ts
@@ -0,0 +1,8 @@
+import {Menu} from './Menu.ts';
+import {Character} from './Character.ts';
+import {Item} from './Item.ts';
+import {Fight} from './Fight.ts';
+import {Gamemanager} from './GameManager.ts';
+import {objects} from './objects.ts';
+import {team} from './team.ts';
+
